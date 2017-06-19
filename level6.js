72ba315a5c28bf46ae64906699b83146831cc19c
Kaleigh Niemela <nkaleigh@users.noreply.github.com>
2017-06-19 12:10:27 -0600

    june19

--- level6.js
+++ level6.js
@@ -29,3 +29,37 @@ for (var j = 0; j < newArr.length; j++) {
 }
 
 duplicateCount('abbccc');
+
+
+
+// Find the minimum absolute difference (MAD) between any two elements in arr.
+
+// Note that the same value can appear more than once in arr. In that case, the MAD will be 0.
+
+// Also, each array has at least two elements and its contains only integers. You can consider the arr as a valid one and do not need to validate it.
+
+function getting_mad(arr) {
+  var answerTemp = [];
+  var answerArr = [];
+  var lowestNum = [];
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      if (i !== j) {
+        answerTemp[0] = arr[i] - arr[j];
+        answerTemp[0] < 0 ? answerArr.push(answerTemp[0] *= -1) : answerArr.push(answerTemp[0]);
+      }
+    }
+  }
+  console.log(answerArr);
+  lowestNum[0] = answerArr.shift(0);
+  console.log(lowestNum);
+  console.log(answerArr);
+  for (var k = 0; k < answerArr.length; k++) {
+    if (lowestNum[0] > answerArr[k]) {
+      lowestNum[0] = answerArr[k];
+    }
+  }
+  return lowestNum[0];
+}
+
+getting_mad([6,0,1,3,6]);
