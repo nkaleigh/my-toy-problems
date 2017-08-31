ab75394bc49ccaefbc55f96316414eb86c559311
Kaleigh Niemela <nkaleigh@users.noreply.github.com>
2017-08-31 10:16:59 -0600

    sumMul

--- level8.js
+++ level8.js
@@ -96,3 +96,37 @@ function arrayMadness(a, b) {
 
 arrayMadness([1,2,3], [2,3,4]);
 
+
+
+//SUM of MULTIPLES
+// ###Your Job:
+
+// Find the sum of the multiples of n i.e. less than m
+
+// ###Keep in Mind:
+
+// n and m are natural number
+// note that the second argument m is excluded from the multiples
+// ###Remember these:
+
+// sumMul(2,9)    should return 20
+// sumMul(3,13)   should return 30
+// sumMul(4,123)  should return 1860
+// sumMul(4,-7)   should return INVALID
+
+function sumMul(n,m){
+  var sum = 0;
+  if (m <= n) {
+    return "INVALID";
+  } else {
+    for (var i = n; i <= m; i++) {
+      if (i % n === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+}
+
+sumMul(0,0);
+
